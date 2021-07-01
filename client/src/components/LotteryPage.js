c1c675aacd76005bf425b737fabf7fda090dc7b9
Sava Stanchev <s.stanchev94@gmail.com>
2021-07-01 17:02:30 +0300

    table with buttons for each of the 80 numbers

--- client/src/components/LotteryPage.js
+++ client/src/components/LotteryPage.js
@@ -2,13 +2,45 @@ import React from 'react';
 import {FaRegPlusSquare, FaRegMinusSquare} from "react-icons/fa";
 
 const LotteryPage = () => {
+  const numbersArr = Array.from({length: 80}, (_, i) => i + 1);
+
+  const getRows = array => {
+    let result = [],
+    i = 0;
+    while (i < array.length) result.push(array.slice(i, i += 10));
+    return result;
+  };
+
+  const rowNumbers = getRows(numbersArr);
+
+  const displayNumbers = rowNumbers.map((row) => {
+    return (
+      <tbody>
+        <tr style={{outline: '#202027 thin solid'}}>
+          {row.map((num) => {
+            return (
+              <td><button className="number-btn">{num}</button></td>
+            )
+          })}
+        </tr>
+      </tbody>
+    )
+  })
 
   return(
     <section className="lottery-main-section">
-      <h1 className="lottery-text">
-        Choose your
-        <span className="accent-text"> numbers!</span>
-      </h1>
+      <div className="songs-container-main-section">
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th colSpan="10">Select 12 numbers!</th>  
+              </tr>
+            </thead>
+            {displayNumbers}
+          </table>
+        </div>
+      </div>
       <form className="lottery-form">
         <div className="input-group">
           <div className="plus-minus">
@@ -20,7 +52,7 @@ const LotteryPage = () => {
               <FaRegMinusSquare/>
             </button>
           </div>
-          <input type="text"/>
+          <input type="text" defaultValue="1.00"/>
           <p className ="reminderMsg">
            * Only numbers with 2 decimal digits
           </p>
@@ -35,7 +67,7 @@ const LotteryPage = () => {
               <FaRegMinusSquare/>
             </button>
           </div>
-          <input type="text"/>
+          <input type="text" defaultValue="1"/>
           <p className ="reminderMsg">
            * Only whole numbers
           </p>
