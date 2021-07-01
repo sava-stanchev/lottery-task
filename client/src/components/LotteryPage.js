79afcf2516ecd9b50697227bcc94dd55f2e1a6bc
Sava Stanchev <s.stanchev94@gmail.com>
2021-07-01 05:29:16 +0300

    jsx and css for input fields, timer and cost

--- client/src/components/LotteryPage.js
+++ client/src/components/LotteryPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaRegPlusSquare, FaRegMinusSquare} from "react-icons/fa";
 
 const LotteryPage = () => {
 
@@ -9,6 +10,41 @@ const LotteryPage = () => {
         <span className="accent-text"> numbers!</span>
       </h1>
       <form className="lottery-form">
+        <div className="input-group">
+          <div className="plus-minus">
+            <label>Bet amount:</label>
+            <button type="button" className="plus-button" tabIndex="-1">
+              <FaRegPlusSquare/>
+            </button>
+            <button type="button" className="minus-button" tabIndex="-1">
+              <FaRegMinusSquare/>
+            </button>
+          </div>
+          <input type="text"/>
+          <p className ="reminderMsg">
+           * Only numbers with 2 decimal digits
+          </p>
+        </div>
+        <div className="input-group">
+          <div className="plus-minus">
+            <label>Number of lottery draws:</label>
+            <button type="button" className="plus-button" tabIndex="-1">
+              <FaRegPlusSquare/>
+            </button>
+            <button type="button" className="minus-button" tabIndex="-1">
+              <FaRegMinusSquare/>
+            </button>
+          </div>
+          <input type="text"/>
+          <p className ="reminderMsg">
+           * Only whole numbers
+          </p>
+        </div>
+        <div className="input-group">
+          <p className ="price-msg">Final cost: 15</p>
+          <p className ="timer">Timer: 02:59</p>
+          <button disabled={true} className="btn">Try your luck!</button>
+        </div>
       </form>
     </section>
   )
